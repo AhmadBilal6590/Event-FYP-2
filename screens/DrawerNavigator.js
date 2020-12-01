9d97e98e9ef3e19d0cfa928c460828dbf2cccfa2
MuhammadAjmalKhan <ajmalkhan50500@gmail.com>
2020-12-01 19:21:56 +0500

    userDone

--- screens/DrawerNavigator.js
+++ screens/DrawerNavigator.js
@@ -14,12 +14,8 @@ const DrawerNavigator = (props) => {
 		<Drawer.Navigator >
 			<Drawer.Screen name="Home" component={Home} initialParams={{ props }} />
 			<Drawer.Screen name="Planning tools" component={PlanningTools} />
-<<<<<<< HEAD
-			<Drawer.Screen name="UserProfile" component={UserProfile} initialParams={{ props }} />
-=======
-            <Drawer.Screen name="UserProfile" component={UserProfile} />
+			<Drawer.Screen name="UserProfile" component={UserProfile} />
 			<Drawer.Screen name="CheckList" component={UserCheckList} />
->>>>>>> 1368e12f0b7a9da98e49a26bfa75d5316cf8e5db
 		</Drawer.Navigator>
 	);
 };
